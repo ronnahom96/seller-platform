c734502eac84a57b49b1195740213e97949fec94
ronnahom96 <ronnahom96@gmail.com>
2023-05-11 08:55:46 +0300

    using dotenv

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import * as configurationProvider from "./common/configuration/configuration-pro
 import { errorHandler } from "./common/errors/error-handler";
 import { logger } from "./common/logger/logger-wrapper";
 import { createServer } from "http";
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const app = getApp();
 const port = configurationProvider.getValue<number>("port");
 const server = createServer(app);
 
-server.listen(port, () => {
+server.listen(port, async () => {
   errorHandler.listenToErrorEvents(server);
   logger.info({ msg: `App started on port ${port}` });
 });
