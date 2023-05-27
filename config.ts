70fed0cb15805b2206e6504af645ae44e048a2e8
ronnahom96 <ronnahom96@gmail.com>
2023-05-27 12:54:12 +0300

    update default port

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export const Configuration: ConfigSchema = {
   port: {
     doc: "The API listening port. By default is 0 (ephemeral) which serves as a dynamic port for testing purposes. For production use, a specific port must be assigned",
     format: "Number",
-    default: 0,
+    default: 3000,
     nullable: true,
     env: "PORT",
   },
