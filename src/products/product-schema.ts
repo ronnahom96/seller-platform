2df84bc90b0341d09a15ffb17c8234bfbdf4b20b
ronnahom96 <ronnahom96@gmail.com>
2023-05-27 12:47:50 +0300

    chore: refactor schema names

--- src/products/product-schema.ts
+++ src/products/product-schema.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import { z } from "zod";
 
 export const ProductSchema = z.object({
   asin: z.string(),
@@ -12,26 +12,18 @@ export const ProductSchema = z.object({
 
 export type Product = z.infer<typeof ProductSchema>;
 
-export type CreateProductRequestBody = z.infer<typeof ProductSchema>;
+export const CreateProductSchema = ProductSchema;
+export type CreateProductRequestBody = z.infer<typeof CreateProductSchema>;
 
 export const UpdateProductSchema = ProductSchema.omit({ asin: true, locale: true }).partial().strict();
-
 export type UpdateProductRequestBody = z.infer<typeof UpdateProductSchema>;
 
-export const ProductIdSchema = z.object({
-  asin: z.string(),
-  locale: z.string(),
-});
+export const ProductIdSchema = ProductSchema.pick({ asin: true, locale: true });
 
-export const ProductsRequestQuerySchema = z.object({
-  sellerName: z.string(),
-  availability: z.boolean()
-})
+export const ProductsRequestQuerySchema = z.object({ sellerName: z.string().min(1), availability: z.boolean().optional() });
+export type ProductsRequestQuery = z.infer<typeof ProductsRequestQuerySchema>;
 
 export type ProductIdParams = z.infer<typeof ProductIdSchema>;
+export const ProductIdsSchema = z.array(ProductIdSchema);
 
-export const ProductIdArraySchema = z.array(ProductIdSchema);
-
-export type ProductBatchDeleteRequestBody = z.infer<typeof ProductIdArraySchema>;
-
-export type ProductsRequestQuery = z.infer<typeof ProductsRequestQuerySchema>;
+export type ProductBatchDeleteRequestBody = z.infer<typeof ProductIdsSchema>;
